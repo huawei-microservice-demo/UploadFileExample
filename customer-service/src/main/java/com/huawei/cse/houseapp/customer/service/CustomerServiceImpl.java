f3f8c5cd5e0fe14faab68f93a76a203ad2af9f8f
bao.liu <bao.liu@huawei.com>
2018-01-16 15:23:17 +0800

    commit example

---
+++ customer-service/src/main/java/com/huawei/cse/houseapp/customer/service/CustomerServiceImpl.java
@@ -0,0 +1,36 @@
+package com.huawei.cse.houseapp.customer.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.servicecomb.provider.rest.common.RestSchema;
+
+@RestSchema(schemaId = "customer")
+@RequestMapping(path = "/")
+public class CustomerServiceImpl {
+  @PostMapping(path = "/upload", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String fileUpload(@RequestPart(name = "file1") MultipartFile file1,
+      @RequestPart(name = "someFile") Part file2) {
+    return _fileUpload(file1, file2);
+  }
+
+  private String _fileUpload(MultipartFile file1, Part file2) {
+    try (InputStream is1 = file1.getInputStream(); InputStream is2 = file2.getInputStream()) {
+      String content1 = IOUtils.toString(is1);
+      String content2 = IOUtils.toString(is2);
+      return file1.getOriginalFilename() + ":" + content1 + "\n" +
+          file2.getSubmittedFileName() + ":" + content2;
+    } catch (IOException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
+}
---
+++ edge-service/src/main/java/com/huawei/cse/houseapp/edge/ApiDispatcher.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huawei.cse.houseapp.edge;
+
+import java.util.Map;
+
+import io.servicecomb.edge.core.AbstractEdgeDispatcher;
+import io.servicecomb.edge.core.CompatiblePathVersionMapper;
+import io.servicecomb.edge.core.EdgeInvocation;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.CookieHandler;
+
+public class ApiDispatcher extends AbstractEdgeDispatcher {
+  private CompatiblePathVersionMapper versionMapper = new CompatiblePathVersionMapper();
+
+  @Override
+  public int getOrder() {
+    return Integer.MIN_VALUE;
+  }
+
+  @Override
+  public void init(Router router) {
+    // api + 微服务名 + 实际路径
+    String regex = "/api/([^\\\\/]+)/(.*)";
+    router.routeWithRegex(regex).handler(CookieHandler.create());
+    router.routeWithRegex(regex).handler(createBodyHandler());
+    router.routeWithRegex(regex).failureHandler(this::onFailure).handler(this::onRequest);
+  }
+
+  protected void onRequest(RoutingContext context) {
+    Map<String, String> pathParams = context.pathParams();
+    String microserviceName = pathParams.get("param0");
+//    String pathVersion = pathParams.get("param1");
+    String path = "/" + pathParams.get("param1");
+
+    EdgeInvocation edgeInvocation = new EdgeInvocation();
+//    edgeInvocation.setVersionRule(versionMapper.getOrCreate(pathVersion).getVersionRule());
+
+    edgeInvocation.init(microserviceName, context, path, httpServerFilters);
+    edgeInvocation.edgeInvoke();
+  }
+}
